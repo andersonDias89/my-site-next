21d03b91ed00aa898ecb3b932c473dfc135e2dbd
Anderson Dias <andersondiasmd25@gmail.com>
2022-06-23 23:01:12 -0300

    finalizando projetos

---
+++ src/components/Projects/Carrousel/Project.tsx
@@ -0,0 +1,31 @@
+type ProjectProps = {
+    title: string
+    description: string
+    image: string
+    repository: string
+    visitPage: string
+}
+
+export function Project({ title, description, image, repository, visitPage}: ProjectProps) {
+    return (
+        <article className="w-full h-96 shape p-4 flex items-center justify-center">
+            <div className="w-4/5 flex">
+                <div className="w-4/6 p-3 relative">
+                    <h2 className="text-xl mb-1">{title}</h2>
+                    <p className="font-light text-blueLight">{description}</p>
+
+                    <div className="space-x-3 absolute bottom-12">
+                        <a href={visitPage} className="degradeButton text-dark font-extrabold shadow-lg transition-colors mt-4 py-2 px-8 rounded-md">Visitar</a>
+                        <a href={repository} className="bg-transparent font-extrabold hover:text-blueLight transition-colors border-2 border-secondary text-secondary mt-4 py-2 px-8 rounded-md">Repositório</a>
+                    </div>
+                </div>
+
+                <div className="w-4/6 p-3">
+                    <img className="rounded-lg" src={image} alt="" />
+                </div>
+
+
+            </div>
+        </article>
+    )
+}
