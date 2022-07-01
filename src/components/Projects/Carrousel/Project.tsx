e08390c5840a840893c3357cd4a1737675abf347
Anderson Dias <anderson@desktop-ti-dev.localdomain>
2022-07-01 11:32:09 -0300

    eliminando botão ir de teste

--- src/components/Projects/Carrousel/Project.tsx
+++ src/components/Projects/Carrousel/Project.tsx
@@ -1,5 +1,3 @@
-import { useRouter } from "next/router"
-
 type ProjectProps = {
     id: string
     title: string
@@ -10,8 +8,6 @@ type ProjectProps = {
 }
 
 export function Project({ title, description, image, repository, visitPage, id}: ProjectProps) {
-    const router = useRouter()
-
     return (
         <article className="w-full h-96 shape p-4 flex items-center justify-center">
             <div className="w-4/5 flex">
@@ -22,12 +18,10 @@ export function Project({ title, description, image, repository, visitPage, id}:
                     <div className="space-x-3 absolute bottom-12">
                         <a href={visitPage}  className="degradeButton text-dark font-extrabold shadow-lg transition-colors mt-4 py-2 px-8 rounded-md">Visitar</a>
                         <a href={repository}  className="bg-transparent font-extrabold hover:text-blueLight transition-colors border-2 border-secondary text-secondary mt-4 py-2 px-8 rounded-md">Repositório</a>
-                        <button type="button" onClick={() => router.push(title)}>Ir</button>
                     </div>
                 </div>
 
                 <div className={`w-4/6 h-64 bg-slate-500 rounded-md ${image} p-3 border-4 border-secondary`}>
-                    {/* <img className="rounded-lg" src={image} alt="" /> */}
                 </div>
 
 
