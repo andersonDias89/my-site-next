82a091fbaefd0f9c44dec4d1e931821fa62bf0ee
Anderson Dias <andersondiasmd25@gmail.com>
2022-07-02 14:04:56 -0300

    config API de envio de e-mail do formulário

--- src/components/Contato/FormContact.tsx
+++ src/components/Contato/FormContact.tsx
@@ -1,10 +1,46 @@
-export function FormContact() {
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+
+export function FormContact({ }) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [mensage, setMensage] = useState('');
+
+
+    const router = useRouter();
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log(name, email, mensage);  
+
+        fetch('/api/email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                mensage
+            })
+        }).then(() => {
+            console.log('Email enviado com sucesso!');   
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    
     return (
-        <form className="flex justify-start items-center w-full flex-col space-y-2  py-6 rounded-md shape">
+        <form onSubmit={handleSubmit} className="flex justify-start items-center w-full flex-col space-y-2  py-6 rounded-md shape">
             <div className="w-4/5 space-y-1">
                 <label className="text-sm" htmlFor="name">Nome:</label>
                 <div className="w-full">
                     <input
+                        required
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
                         type="text"
                         name="name"
                         placeholder="digite o seu nome"
@@ -17,6 +53,9 @@ export function FormContact() {
                 <label className="text-sm" htmlFor="email">E-mail:</label>
                 <div className="w-full">
                     <input
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
                         type="email"
                         name="email"
                         placeholder="digite o seu e-mail"
@@ -29,6 +68,9 @@ export function FormContact() {
                 <label className="text-sm" htmlFor="email">Digite sua mensagem:</label>
                 <div className="w-full h-40">
                     <textarea
+                        required
+                        onChange={(e) => setMensage(e.target.value)}
+                        value={mensage}
                         name="message"
                         className="w-full rounded-lg p-2 outline-0 bg-dark placeholder-gray-700 resize-none h-full"
                     />
