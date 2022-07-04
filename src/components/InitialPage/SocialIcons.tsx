325c4510899404a8ba6ee6fa006b56c4ccae1d88
Anderson Dias <anderson@desktop-ti-dev.localdomain>
2022-07-04 09:11:25 -0300

    adicionado os links das redes socias

--- src/components/InitialPage/SocialIcons.tsx
+++ src/components/InitialPage/SocialIcons.tsx
@@ -1,19 +1,28 @@
+import Link from "next/link";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
 export function SocialIcons() {
     return (
         <div className="w-full container flex justify-center">
             <div className="animation-transition-top z-50 flex space-x-4 text-4xl text-secondary">
-                <a className="hover:-translate-y-1 transition-transform cursor-pointer">
-                    <AiFillGithub />
-                </a>
-                <a className="hover:-translate-y-1 transition-transform cursor-pointer">
-                    <AiFillLinkedin />
-                </a>
+                <Link href={'https://github.com/andersonDias89'}>
+                    <a className="hover:-translate-y-1 transition-transform cursor-pointer">
+                        <AiFillGithub />
+                    </a>
+                </Link>
 
-                <a className="hover:-translate-y-1 transition-transform cursor-pointer">
-                    <AiFillInstagram />
-                </a>
+                <Link href={'https://www.linkedin.com/in/anderson-dias-bb7b3122b/'}>
+                    <a className="hover:-translate-y-1 transition-transform cursor-pointer">
+                        <AiFillLinkedin />
+                    </a>
+                </Link>
+
+                <Link href={'https://www.instagram.com/andersondias_dev'}>
+                    <a className="hover:-translate-y-1 transition-transform cursor-pointer">
+                        <AiFillInstagram />
+                    </a>
+                </Link>
+
             </div>
         </div>
     )
