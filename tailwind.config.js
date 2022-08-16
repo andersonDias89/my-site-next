05a9d23cac1c2e0b85468c464a29c8641d4925ff
Anderson Dias <andersondiasmd25@gmail.com>
2022-08-15 21:36:54 -0300

    feat: definindo cores globais

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-     "./src/pages/**/*.{js,ts,jsx,tsx}",
-     "./src/components/**/*.{js,ts,jsx,tsx}"
-   ],
-   theme: {
-     extend: {
-       colors: {
-         
-       },
-       screens: {
-          
-          
-       }
-     },
-   },
-   plugins: [],
- }
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-bg": "#18181B",
+        "secondary-bg": "#1B1B1F",
+        "card-bg": "#28282E",
+        "primary": "#8234BD",
+        "secondary": "#4D9E8E",
+        "tertiary": "#4A255C"
+      },
+      screens: {
+
+
+      }
+    },
+  },
+  plugins: [],
+}
