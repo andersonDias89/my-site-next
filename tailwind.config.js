cd36592884f13e04dd47887ff69fab52ab0e979e
Anderson Dias <andersondiasmd25@gmail.com>
2022-08-15 21:01:28 -0300

    inicio realese

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@
 module.exports = {
   content: [
      "./src/pages/**/*.{js,ts,jsx,tsx}",
-     "./src/components/**/*.{js,ts,jsx,tsx}",
+     "./src/components/**/*.{js,ts,jsx,tsx}"
    ],
    theme: {
      extend: {
        colors: {
-         dark: "#13131F", 
-         white: "#FEFBFB",
-         secondary: "#05da9a",
-         blueLight: "#05da9a",
-         purple: "#5e58b1"
+         
+       },
+       screens: {
+          
+          
        }
      },
    },
