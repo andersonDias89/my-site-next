788b882804e378e97c1b289ab825d0a88b0795e1
Anderson Dias <andersondiasmd25@gmail.com>
2022-08-17 19:51:59 -0300

    refactor: add button no tittleHome

--- src/components/Initial/TitleHome.tsx
+++ src/components/Initial/TitleHome.tsx
@@ -1,8 +1,26 @@
+import { Button } from "../../common/Button";
+import { Browsers, ChatCircleDots } from 'phosphor-react'
+
 export function TitleHome() {
     return (
-        <div>
+        <div className="flex flex-col">
             <h1 className="font-black text-[52px]">Anderson Dias</h1>
-            <span className="tracking-widest text-[16px]">Desenvolvedor front-end | UX/UI Design</span>
+            <span className="tracking-widest -mt-3 text-[16px]">Desenvolvedor front-end | UX/UI Design</span>
+            <div className="flex gap-4 mt-4">
+                <Button
+                    type="button"
+                    title="Projetos"
+                    icon={<Browsers size={24} weight="fill" />}
+                    style="buttonPrimary"
+                />
+
+                <Button
+                    type="button"
+                    title="Contato"
+                    icon={<ChatCircleDots size={24} weight="fill" />}
+                    style="buttonSecondary"
+                />
+            </div>
         </div>
     )
 }
